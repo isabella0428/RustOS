fc0b708b11f1ae67ba011737428ae6648c69996a
isabella0428 <isabella_aus_china@163.com>
2023-02-12 14:52:06 -0500

    Add hardware interrupt support

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,7 +1,35 @@
 use crate::println;
+use crate::gdt;
+use crate::print;
+
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
-use crate::gdt;
+use pic8259::ChainedPics;
+use spin;
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+}
+
+impl InterruptIndex{
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
+}
+
+
+pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(unsafe {
+    {ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET)}
+});
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -12,8 +40,17 @@ lazy_static! {
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
         idt
     };
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    print!(".");
+
+    unsafe {
+        PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
 }
 
 pub fn init_idt() {
--- src/main.rs
+++ src/main.rs
@@ -51,10 +51,12 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    }
+
     // trigger a page fault
-    unsafe {
-        *(0xdeadbeef as *mut u64) = 42;
-    };
+    //stack_overflow();
 
     // as before
     #[cfg(test)]
